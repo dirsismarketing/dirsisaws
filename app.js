fccd3c52137f3b8c0d0a8df2c0c1364136b0db0d
dirsismarketing <dirsis@gmail.com>
2024-02-05 00:02:11 -0300

    upd2

---
+++ app.js
@@ -0,0 +1,62 @@
+const telefonos = require("./agenda.js");
+buscarportelefono("39-")
+function buscarportelefono(telefono){
+    const contacto = telefonos.filter((x) => x.number.startsWith(telefono));
+    let contactoclon={...contacto}git init
+    console.log(contacto);
+}
+
+
+/*
+function mostrarContacto(id){
+    const contacto = telefonos.find((x) => x.id === id);
+    console.log(contacto);
+}
+
+/*
+borrarcontacto("351-3807748");
+function borrarcontacto(nro){
+    telefonos2 = telefonos.filter((x) => x.number != nro);
+    console.log(telefonos2);
+}
+/*
+let nuevo = {
+    id: 8,
+    name: "Diego",
+    number: "351-3807748",
+};
+agregarobjeto(nuevo);
+function agregarobjeto(nuevo){
+    telefonos.push(nuevo);
+    console.log(telefonos);
+}
+
+//cantidadtelefono();
+//mostrartelefonos();
+//mostrarContacto(2);
+/*
+function cantidadtelefono(){
+    console.log(telefonos.length);
+}
+
+function mostrartelefonos(){
+    console.log(telefonos);
+}
+
+
+
+
+*/
+
+/*
+const Chance = require("chance");
+const chance = new Chance();
+
+const randomEmail = chance.email();
+const randomAge = chance.age();
+const randomName = chance.name();
+
+console.log(randomAge,randomEmail,randomName);
+*/
+
+
