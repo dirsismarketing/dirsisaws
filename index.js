340ea5647c8af03f9b2286a8d834917aaa20ab57
dirsismarketing <dirsis@gmail.com>
2024-02-05 00:16:54 -0300

    upd2

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const books = require("./libros");
+
+app.use("/api/books", (req, res) => {
+    res.status(200).json({estado: "OK", result: books});
+    const book = {
+        title: req.body.title,
+        author: req.body.author,
+        year: req.body.year,
+        id: books.length + 1,
+    }
+    books.push(book);
+});
+
+app.use("/api/books/:id", (req, res) => {
+    console.log("entro");
+    const id = req.params.id;
+    const libro = books.find ((x)=> x.id == id);
+    res.status(200).json({estado: "OK", result: libro});
+});
+
+app.use("/api/books", (req, res) => {
+    res.status(200).json({estado: "OK", result: books});
+});
+
+app.listen(port, () => {
+    console.log("la aplicacion es corriendo ");
+});
+
---
+++ libros.js
@@ -0,0 +1,67 @@
+const books = [
+    {
+      "title": "Dune",
+      "author": "Frank Herbert",
+      "year": "1965",
+      "id": "1"
+    },
+    {
+      "title": "1984",
+      "author": "George Orwell",
+      "year": "1949",
+      "id": "2"
+    },
+    {
+      "title": "Neuromancer",
+      "author": "William Gibson",
+      "year": "1984",
+      "id": "3"
+    },
+    {
+      "title": "The Left Hand of Darkness",
+      "author": "Ursula K. Le Guin",
+      "year": "1969",
+      "id": "4"
+    },
+    {
+      "title": "Snow Crash",
+      "author": "Neal Stephenson",
+      "year": "1992",
+      "id": "5"
+    },
+    {
+      "title": "Ender's Game",
+      "author": "Orson Scott Card",
+      "year": "1985",
+      "id": "6"
+    },
+    {
+      "title": "Hyperion",
+      "author": "Dan Simmons",
+      "year": "1989",
+      "id": "7"
+    },
+    {
+      "title": "Do Androids Dream of Electric Sheep?",
+      "author": "Philip K. Dick",
+      "year": "1968",
+      "id": "8"
+    },
+    {
+      "title": "The Hitchhiker's Guide to the Galaxy",
+      "author": "Douglas Adams",
+      "year": "1979",
+      "id": "9"
+    },
+    {
+      "title": "Foundation",
+      "author": "Isaac Asimov",
+      "year": "1951",
+      "id": "10"
+    }
+  ];
+
+  module.exports = books;
+
+
+  
